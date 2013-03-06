7e6e48ada71cb2c76fe879f99d7678c34ffeff12
Trond <krognar@gmail.com>
2013-03-06 15:04:29 +0100

    menu-elements expand with dummy sub-menus when clicked

--- UI/scripts/menu-elements.js
+++ UI/scripts/menu-elements.js
@@ -1,20 +1,33 @@
-$(".clickable").click(function(){
-    if(this.classList.contains("selected")){
-        removeMenu(this);
-    } else {
-        addMenu(this);
-    }
+$(document).ready(function(){
+	$(".clickable").click(function(){
+	    if(this.classList.contains("selected")){
+		removeMenu(this);
+	    } else {
+		addMenu(this);
+	    }
+	});
 });
 
 
 function addMenu(elm){
     $(".selected").each(function(index, element) {
-        element.classList.remove("selected");
+    	removeMenu(element);
     });    
     elm.classList.add("selected");
+    var menuDiv = document.createElement("div");
+    menuDiv.id = "menuDiv";
+    menuDiv.innerHTML = createMenuDiv(this);
+    elm.appendChild(menuDiv);
 }
 
 
 function removeMenu(elm){
+    var rem = document.getElementById("menuDiv");
+    elm.removeChild(rem);
     elm.classList.remove("selected");
+}
+
+// Dummy-function, needs to add proper elements that expand and work.
+function createMenuDiv(elm){
+	return "<hr /> Test 01 <hr /> Test 02 <hr /> Test 03 <hr /> Cancel <hr /><hr />";
 }
