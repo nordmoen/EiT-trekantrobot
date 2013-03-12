0f28e29c3eb0b0b963c464d7e9128c53fdd0d22e
Jørgen Nordmoen <nordmoen90@gmail.com>
2013-03-12 08:50:10 +0100

    Removed the effect on the canvas because of usability conserns. Added alerts to comm to inform the user further when something unexpected happens

--- UI/scripts/comm.js
+++ UI/scripts/comm.js
@@ -9,26 +9,39 @@ $(document).ready(function(){
 	socket.on('connect_failed', function () {
 		setBannerText("Could not connect to server", -1);
 		globalSocket = null;
-		//TODO: Make an overlay or something to tell the user to reload
+		alert('The connection to the server could not be established. ' +
+			'This could mean that the server is busy with someone ' +
+			'else or that the server could not be reached. Try ' +
+			'to reload the page and if the problem persists, try ' +
+			'to restart the server');
 	});
 	socket.on('disconnected', function () {
-		setBannerText("Disconected from server", 0);
+		setBannerText("Disconected from server", -1);
 		gloabalSocket = null;
-		//TODO: Make an overlay or something to tell the user to reload
+		alert('The connection was disrupted, if the page does not ' +
+			'try to reconnect try and reload the page, if that does ' +
+			'not help restart the server');
 	});
 	socket.on('reconnecting', function () {
 		setBannerText("Trying to reconnect to server", -1);
 		gloabalSocket = null;
-		//TODO: Make an overlay or something to tell the user to reload
+		alert('The page is trying to reconnect to the server, during ' +
+			'this time the page can not be used and all commands ' +
+			'will timeout. If the page is not reconnected within ' +
+			'a minute or so check your network connection and try ' +
+			'to reload the page');
 	});
 	socket.on('reconnect', function () {
-		setBannerText("Connected successfully to server", 1000);
+		setBannerText("Reconnected successfully to server", 1000);
 		gloabalSocket = socket;
 	});
 	socket.on('error', function () {
 		setBannerText("The server connection had an accident", -1);
 		globalSocket = null;
-		//TODO: Make an overlay or something to tell the user to reload
+		alert('Some major mishap just happened, if this is the first time ' +
+			'you see this message try and reload the page and pretend ' +
+			'that you did not see this, if however this has happened ' +
+			'multiple times run around with your arms flailing and yell ' +
+			'"THE WORLD IS ABOUT TO END!"');
 	});
 });
-
