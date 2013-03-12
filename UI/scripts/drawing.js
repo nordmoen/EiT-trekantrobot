7b62495fe2c7f71c9f0823bc0569a1b56b3f9255
Jørgen Nordmoen <nordmoen90@gmail.com>
2013-03-12 10:51:23 +0100

    Added kintect js and started the drawing functions. Changed Index to reflect that we moved away from HTML5 Canvas

---
+++ UI/scripts/drawing.js
@@ -0,0 +1,26 @@
+function drawRobot(id, x, y, direction) {
+	var r = globalLayer.get("#" + id);
+	if (r.length === 0){
+		var rWedge = new Kinetic.Wedge({
+			x: x,
+			y: y,
+			radius: 70,
+			angleDeg: 60,
+			stroke: 'blue',
+			strokeWidth: 3,
+			rotationDeg: direction,
+			id: id
+		});
+		globalLayer.add(rWedge);
+		globalStage.add(globalLayer);
+	}else{
+		move(id, x, y, direction);
+	}
+}
+
+function move(id, x, y, direction){
+	var robo = globalLayer.get("#" + id);
+	robo.setX(x);
+	robo.setY(y);
+	robo.setRotationDeg(direction);
+}
